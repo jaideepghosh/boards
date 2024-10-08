0fad726b9dcdf346c093c0708378605019df675e
jaideepghosh <er.jaideepghosh@gmail.com>
2024-10-08 15:02:28 +0530

    feat:Added shadcn toast component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -15,8 +16,13 @@ const config: Config = {
         secondary: "var(--dark-mint)",
         light: "var(--dawn-pink)",
       },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
